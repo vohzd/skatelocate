14c146c3710a12c7e39fc533dc337a6a838bd6c3
Ben <vohzd@protonmail.com>
2017-08-24 02:18:10 +0100

    images

--- src/components/DetailsPanel.vue
+++ src/components/DetailsPanel.vue
@@ -1,17 +1,17 @@
 <template lang="html">
   <section class="details-panel">
     <div  v-if="focused">
+
       <h4>{{ focused.skateparkName }}</h4>
-      <p>{{ focused.skateparkDesc }}</p>
-      <time>{{ focused.createdAt }}</time>
-      <p>{{ focused.skateparkAdder }}</p>
-      <ul>
-        <li v-for="tag in focused.skateparkTags">
-          {{ tag }}
-        </li>
-      </ul>
-      <div v-for="imgSrc in focused.skateparkImages">
-        {{ imgSrc }}
+      <p class="skatepark-desc">{{ focused.skateparkDesc }}</p>
+      <span class="added">Added on {{ focused.createdAt | dateFilter }} by {{ focused.skateparkAdder }}</span>
+      <div class="tags">
+        <div v-for="tag in focused.skateparkTags" class="tag">{{ tag }}</div>
+      </div>
+      <div class="scrolling">
+        <div v-for="imgSrc in focused.skateparkImages" class="side-by-side">
+          <img v-bind:src="imgSrc" class="fixed-height"/>
+        </div>
       </div>
     </div>
   </section>
@@ -20,6 +20,8 @@
 <script>
 
 import { mapGetters } from "Vuex";
+
+import moment         from "moment";
 
 export default {
   data(){
@@ -32,6 +34,11 @@ export default {
       "skateparks",
       "skateparkInFocus"
     ])
+  },
+  filters: {
+    dateFilter(timestamp) {
+      return moment(timestamp).format("Do MMMM YYYY");
+    }
   },
   watch: {
     skateparkInFocus(){
@@ -52,7 +59,53 @@ export default {
     z-index: 6000;
     top: 0;
     right: 0;
-    width: 35%;
+    width: 44%;
+    padding: 12px;
+  }
+
+  .details-panel h4 {
+    font-size: 30px;
+  }
+
+  .details-panel .skatepark-desc {
+    font-size: 16px;
+  }
+
+  .details-panel .added {
+    font-size: 13px;
+  }
+
+  .details-panel .tags {
+    float: left;
+    margin-top: 32px;
+    width: 100%;
+  }
+
+  .details-panel .tag {
+    font-size: 12px;
+    padding: 6px;
+    margin-right: 6px;
+    margin-bottom: 6px;
+    float: left;
+    border-radius: 10px;
+    background: rgba(0,0,0,0.06);
+  }
+
+  .details-panel .scrolling {
+    margin-top: 32px;
+    width: 100%;
+    overflow-y: scroll;
+    float: left;
+    display: flex;
+    bottom: 0;
+  }
+
+  .details-panel .side-by-side {
+    float: left;
+  }
+
+  .fixed-height {
+    height: 330px;
   }
 
 </style>
