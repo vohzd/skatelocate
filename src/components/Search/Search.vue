5c2381d1ee500f7145df5ffc2695e3fe2addaba1
Ben <vohzd@protonmail.com>
2017-09-18 23:46:15 +0100

    add search frame

--- src/components/Search/Search.vue
+++ src/components/Search/Search.vue
@@ -5,7 +5,21 @@
       <h3>SEARCH</h3>
     </header>
     <section class="section-content">
-      <p>Content here</p>
+      <div class="text-search">
+        <i class="fa fa-search" aria-hidden="true"></i>
+        <input type="text" placeholder="Search by phrase..." />
+      </div>
+      <div class="tag-search">
+        <i class="fa fa-tags" aria-hidden="true"></i>
+        <div class="tags">
+          <div v-for="tag in availableSkateparkTags" class="tag">
+            {{ tag }}
+          </div>
+        </div>
+      </div>
+      <div class="results">
+        searching...
+      </div>
     </section>
   </section>
 </template>
@@ -18,6 +32,11 @@ export default {
   components: {
     "toggle-nav-panel": toggleNavPanel
   },
+  computed: {
+    ...mapGetters([
+      "availableSkateparkTags"
+    ])
+  },
   data(){
     return {
     }
@@ -29,5 +48,50 @@ export default {
 </script>
 
 <style lang="css">
+
+  .text-search {
+    float: left;
+    width: 100%;
+    margin-bottom: 16px;
+  }
+
+  .tag-search {
+    width: 100%;
+    float: left;
+  }
+
+  .tag-search .tags{
+    float: left;
+    width: calc(100% - 64px);
+    margin-left: 16px;
+  }
+  .text-search i, .tag-search i{
+    width: 32px;
+    font-size: 32px;
+    float: left;
+  }
+  .text-search input  {
+    margin-left: 16px;
+    padding: 8px;
+    font-size: 22px;
+    line-height: 22px;
+    width: calc(100% - 64px);
+    float: left;
+    border: 0;
+    outline: 0;
+    background: white;
+    font-family: "kalam";
+  }
+
+  .results {
+    padding: 8px;
+    width: calc(100% - 16px);
+    margin-top: 32px;
+    background: rgba(0,0,0,0.06);
+    color: #444;
+    font-size: 18px;
+    border: 1px solid #DDDDDD;
+    float: left;
+  }
 
 </style>
